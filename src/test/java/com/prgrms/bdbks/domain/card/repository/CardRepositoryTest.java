0d6a7cc28cdab0cfc3e094aedf8c60314a85249d
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-19 10:49:43 +0900

    test : findByUserId 테스트 추가

---
+++ src/test/java/com/prgrms/bdbks/domain/card/repository/CardRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.prgrms.bdbks.domain.card.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.prgrms.bdbks.CustomDataJpaTest;
+import com.prgrms.bdbks.domain.card.entity.Card;
+import com.prgrms.bdbks.domain.user.entity.User;
+import com.prgrms.bdbks.domain.user.repository.UserRepository;
+import com.prgrms.bdbks.domain.user.role.Role;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@CustomDataJpaTest
+class CardRepositoryTest {
+
+	private User getUser() {
+		return User.builder()
+			.id(1L)
+			.loginId("asdfasdf")
+			.password("asdfasdf")
+			.nickname("asdfasdf")
+			.email("asdfas@naver.com")
+			.birthDate(LocalDateTime.now().minusHours(5L))
+			.phone("01012341234")
+			.role(Role.USER)
+			.build();
+	}
+
+	private Card getCard() {
+		return Card.builder()
+			.user(getUser())
+			.build();
+	}
+
+	@BeforeEach()
+	void setUp() {
+		userRepository.save(getUser());
+		cardRepository.save(getCard());
+	}
+
+	@Autowired
+	CardRepository cardRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Test
+	void queryTest() {
+		User user = getUser();
+		log.info("================findById==============");
+		List<Card> result = cardRepository.findByUserId(user.getId());
+		System.out.println(result.get(0).getId());
+
+	}
+	
+}
