e2dfef5f684383eebd51bbd47a29e223949410e8
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-01-21 17:57:49 +0900

    test : CardRequest static 생성 메서드 추가

--- src/test/java/com/prgrms/bdbks/domain/testutil/CardObjectProvider.java
+++ src/test/java/com/prgrms/bdbks/domain/testutil/CardObjectProvider.java
@@ -1,15 +1,28 @@
 package com.prgrms.bdbks.domain.testutil;
 
+import com.prgrms.bdbks.domain.card.dto.CardChargeRequest;
 import com.prgrms.bdbks.domain.card.entity.Card;
 import com.prgrms.bdbks.domain.user.entity.User;
 
 public class CardObjectProvider {
 
-	public static Card createCard(User user) {
-		return Card.builder()
-			.user(user)
-			.name("카드카드카드")
-			.build();
-	}
+    public static Card createCard(User user) {
+        return Card.builder()
+                .user(user)
+                .name("카드카드카드")
+                .build();
+    }
+
+    public static Card createCard(User user, String id) {
+        return Card.builder()
+                .id(id)
+                .user(user)
+                .name("카드카드카드")
+                .build();
+    }
+
+    public static CardChargeRequest createCardRequest(String id, int amount) {
+        return new CardChargeRequest(id, amount);
+    }
 
 }
