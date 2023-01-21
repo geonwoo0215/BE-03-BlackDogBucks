c9ece85d3e48e16e3b5937ac6e83a81fca3a8cf3
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-01-21 17:32:01 +0900

    refactor : 파라미터 id -> property 수정

--- src/main/java/com/prgrms/bdbks/common/exception/EntityNotFoundException.java
+++ src/main/java/com/prgrms/bdbks/common/exception/EntityNotFoundException.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class EntityNotFoundException extends BusinessException {
 
-	public EntityNotFoundException(Class<?> entity, Object id) {
-		super(String.format("%s 엔티티를 찾을 수 없습니다. by id : %s", entity.getSimpleName(), id.toString()));
+	public EntityNotFoundException(Class<?> entity, Object property) {
+		super(String.format("%s 엔티티를 찾을 수 없습니다. by id : %s", entity.getSimpleName(), property.toString()));
 	}
 
 	public EntityNotFoundException(Class<?> entity, Object... properties) {
