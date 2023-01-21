1c191e31ec6734253d3549f6405a2515267a5392
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-01-21 17:36:47 +0900

    refactor : CardChargeRequest 수정 - 필드 final 삭제 - NoArgsConstructor 추가

--- src/main/java/com/prgrms/bdbks/domain/card/dto/CardChargeRequest.java
+++ src/main/java/com/prgrms/bdbks/domain/card/dto/CardChargeRequest.java
@@ -4,18 +4,21 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CardChargeRequest {
 
 	@NotBlank(message = "충전할 카드의 ID를 입력해주세요.")
-	private final String cardId;
+	private String cardId;
 
 	@Min(value = 10000, message = "최소 충전 금액은 10000원 입니다.")
 	@Max(value = 550000, message = "최대 충전 금액은 550000원 입니다.")
-	private final int amount;
+	private int amount;
 
 }
