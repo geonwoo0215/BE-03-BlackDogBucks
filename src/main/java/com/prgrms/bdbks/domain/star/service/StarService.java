c27bc9417d018a9d0c2edb1c6d11ff4b3fa4b63a
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-24 18:27:18 +0900

    feat : Star 생성, 조회, 삭제 기능 추가

---
+++ src/main/java/com/prgrms/bdbks/domain/star/service/StarService.java
@@ -0,0 +1,49 @@
+package com.prgrms.bdbks.domain.star.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.prgrms.bdbks.common.exception.EntityNotFoundException;
+import com.prgrms.bdbks.domain.star.dto.StarSearchResponse;
+import com.prgrms.bdbks.domain.star.entity.Star;
+import com.prgrms.bdbks.domain.star.mapper.StarMapper;
+import com.prgrms.bdbks.domain.star.repository.StarRepository;
+import com.prgrms.bdbks.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StarService {
+
+	private static final short ZERO = 0;
+
+	private final StarRepository starRepository;
+	private final StarMapper starMapper;
+
+	@Transactional
+	public Long create(User user) {
+		Star star = Star.builder()
+			.user(user)
+			.count(ZERO)
+			.build();
+
+		starRepository.save(star);
+		return star.getId();
+	}
+
+	public StarSearchResponse findById(Long userId) {
+		Star star = starRepository.findByUserId(userId)
+			.orElseThrow(() -> new EntityNotFoundException(Star.class, userId));
+
+		return starMapper.toStarSearchResponse(star);
+	}
+
+	@Transactional
+	public void delete(Long userId) {
+		starRepository.deleteByUserId(userId);
+	}
+
+	//TODO 거래 취소, 반품 시 별은 원상복구(원래 상태로 감소)
+}
