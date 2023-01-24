5eac0b21ad6498335b486a37a5a367259260beb1
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-01-24 18:20:35 +0900

    chore : 패키지 변경

--- src/main/java/com/prgrms/bdbks/domain/coupon/service/CouponService.java
+++ src/main/java/com/prgrms/bdbks/domain/coupon/service/CouponService.java
@@ -1,46 +1,48 @@
 package com.prgrms.bdbks.domain.coupon.service;
 
-import com.prgrms.bdbks.domain.coupon.dto.CouponSaveResponse;
-import com.prgrms.bdbks.domain.coupon.dto.CouponSearchResponses;
-import com.prgrms.bdbks.domain.coupon.entity.Coupon;
-import com.prgrms.bdbks.domain.coupon.repository.CouponRepository;
-import com.prgrms.bdbks.domain.coupon.service.mapper.CouponMapper;
-import com.prgrms.bdbks.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.prgrms.bdbks.domain.coupon.dto.CouponSaveResponse;
+import com.prgrms.bdbks.domain.coupon.dto.CouponSearchResponses;
+import com.prgrms.bdbks.domain.coupon.entity.Coupon;
+import com.prgrms.bdbks.domain.coupon.mapper.CouponMapper;
+import com.prgrms.bdbks.domain.coupon.repository.CouponRepository;
+import com.prgrms.bdbks.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CouponService {
 
-    private final CouponMapper couponMapper;
-    private final CouponRepository couponRepository;
+	private final CouponMapper couponMapper;
+	private final CouponRepository couponRepository;
 
-    public CouponSaveResponse create(User user) {
+	public CouponSaveResponse create(User user) {
 
-        Coupon coupon = Coupon.builder()
-                .userId(user.getId())
-                .name(user.getNickname() + "의 쿠폰")
-                .price(6000)
-                .expireDate(LocalDateTime.now().plusMonths(6L))
-                .build();
+		Coupon coupon = Coupon.builder()
+			.userId(user.getId())
+			.name(user.getNickname() + "의 쿠폰")
+			.price(6000)
+			.expireDate(LocalDateTime.now().plusMonths(6L))
+			.build();
 
-        Coupon saveCoupon = couponRepository.save(coupon);
+		Coupon saveCoupon = couponRepository.save(coupon);
 
-        return couponMapper.toCouponSaveResponse(saveCoupon);
-    }
+		return couponMapper.toCouponSaveResponse(saveCoupon);
+	}
 
-    public CouponSearchResponses getCouponList(User user) {
+	public CouponSearchResponses getCouponList(User user) {
 
-        List<Coupon> coupons = couponRepository.findByUserId(user.getId());
+		List<Coupon> coupons = couponRepository.findByUserId(user.getId());
 
-        return CouponSearchResponses.of(
-                coupons.stream()
-                        .map(couponMapper::toCouponSearchResponse)
-                        .collect(Collectors.toList()));
-    }
+		return CouponSearchResponses.of(
+			coupons.stream()
+				.map(couponMapper::toCouponSearchResponse)
+				.collect(Collectors.toList()));
+	}
 }
